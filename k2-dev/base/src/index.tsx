8db471ad09b6f23a3c955ce420da8dfb5e30da91
Alexandr Dubinin <akiyamka@gmail.com>
2020-02-24 19:04:10 +0300

    feat: initial working variant

--- k2-dev/base/src/index.tsx
+++ k2-dev/base/src/index.tsx
@@ -1,6 +1,11 @@
 import React, { Suspense } from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Loadable from 'react-loadable';
+import store from './store';
+
+// Modules:
 import Module from '@k2-packages/module';
 /* !not-delete! cli:import */
 
@@ -10,26 +15,39 @@ const LoadingState = (
     </div>
 );
 
+const LazyGeocoder = Loadable({
+    loader: () => import('@k2-packages/geocoder'),
+    loading: () => <div>Loading Scripts...</div>,
+});
+
 const BasicRouting = () => (
     <Suspense fallback={LoadingState}>
-        <h2>Base app111</h2>
-        <Router>
-            <div>
-                <ul>
-                    <li>
-                        <Link to="/">Module</Link>
-                    </li>
-                    {/* !not-delete! cli:link */}
-                </ul>
-                <hr />
-                <Switch>
-                    <Route exact path="/">
-                        <Module />
-                    </Route>
-                    {/* !not-delete! cli:route */}
-                </Switch>
-            </div>
-        </Router>
+        <Provider store={store}>
+            <h2>Base app</h2>
+            <Router>
+                <div>
+                    <ul>
+                        <li>
+                            <Link to="/">Module</Link>
+                        </li>
+                        <li>
+                            <Link to="/geocoder">geocoder</Link>
+                        </li>
+                        {/* !not-delete! cli:link */}
+                    </ul>
+                    <hr />
+                    <Switch>
+                        <Route exact path="/">
+                            <Module />
+                        </Route>
+                        <Route path="/geocoder">
+                            <LazyGeocoder />
+                        </Route>
+                        {/* !not-delete! cli:route */}
+                    </Switch>
+                </div>
+            </Router>
+        </Provider>
     </Suspense>
 );
 
---
+++ k2-dev/base/src/modulesProvider.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Loadable from 'react-loadable';
+
+const loadedModulesCache = {};
+
+export function LazyModule({ path }) {
+  if (!loadedModulesCache[path]) {
+    loadedModulesCache[path] = Loadable({
+      loader: () => import('@k2-packages/' + path),
+      loading: () => <div>Loading Module...</div>
+    });
+  }
+
+  const LoadableModule = loadedModulesCache[path];
+  return <LoadableModule />
+}

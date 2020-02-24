6303feb8429da8735c310f090b5144e2563e0caf
Alexandr Dubinin <akiyamka@gmail.com>
2020-02-24 14:35:24 +0300

    feat: update plop script in readme added `lerna link` command after `lerna add`. Implement this changes in plop too

--- k2-dev/base/src/index.tsx
+++ k2-dev/base/src/index.tsx
@@ -1,7 +1,6 @@
 import React, { Suspense } from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
-// import Dummy from '@k2-packages/dummy';
 import Module from '@k2-packages/module';
 /* !not-delete! cli:import */
 
@@ -20,18 +19,12 @@ const BasicRouting = () => (
                     <li>
                         <Link to="/">Module</Link>
                     </li>
-                    <li>
-                        <Link to="/dummy">Dummy</Link>
-                    </li>
                     {/* !not-delete! cli:link */}
                 </ul>
                 <hr />
                 <Switch>
                     <Route exact path="/">
                         <Module />
-                    </Route>
-                    <Route path="/dummy">
-                        {/*<Dummy />*/}
                     </Route>
                     {/* !not-delete! cli:route */}
                 </Switch>

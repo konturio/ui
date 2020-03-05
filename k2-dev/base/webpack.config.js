9d1d2ab3f9be20a939b102f2a4cc1dabfa7cc924
Ilya Kvachenko <ikvachenko@objectstyle.com>
2020-03-05 17:00:20 +0300

    proxy update

--- k2-dev/base/webpack.config.js
+++ k2-dev/base/webpack.config.js
@@ -1,5 +1,9 @@
+const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
+
+console.log('MODE:', process.env.NODE_ENV);
+console.log('API:', process.env.API);
 
 module.exports = {
   entry: {
@@ -38,6 +42,10 @@ module.exports = {
     contentBase: path.resolve(__dirname, '/dist')
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': `'${process.env.NODE_ENV}'`,
+      'process.env.API': `'${process.env.API}'`,
+    }),
     new HtmlWebPackPlugin({
       template: './src/index.html',
       filename: 'index.html',

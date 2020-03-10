88f73c6f2fbb7b2a70dc503280a0eadf6981d11d
Alexandr Dubinin <akiyamka@gmail.com>
2020-03-10 20:21:13 +0300

    feat: move input to lib

--- k2-dev/base/src/index.tsx
+++ k2-dev/base/src/index.tsx
@@ -8,65 +8,65 @@ import './index.scss';
 // Modules:
 import Module from '@k2-packages/module';
 import Geocoder from '@k2-packages/geocoder';
-import Input from '@k2-packages/input';
+import { Input } from '@k2-packages/ui-kit';
 /* !not-delete! cli:import */
 
 const LoadingState = (
-    <div style={{ background: 'aqua', width: '100px', height: '100px' }}>
-        Loading...
+  <div style={{ background: 'aqua', width: '100px', height: '100px' }}>
+    Loading...
     </div>
 );
 
 const BasicRouting = () => (
-    <Suspense fallback={LoadingState}>
-        <Provider store={store}>
-            <h2>Base app</h2>
-            <Router>
-                <div>
-                    <ul>
-                        <li>
-                            <Link to="/">Module</Link>
-                        </li>
-                        <li>
-                            <Link to="/geocoder">geocoder</Link>
-                        </li>
-                        <li>
-                            <Link to="/kit">UI Kit</Link>
-                        </li>
-                        {/* !not-delete! cli:link */}
-                    </ul>
-                    <hr />
-                    <Switch>
-                        <Route exact path="/">
-                            <Module />
-                        </Route>
-                        <Route path="/geocoder">
-                            <Geocoder />
-                        </Route>
-                        <Route path="/kit">
-                            <form className="inputs">
-                                <div>Default</div>
-                                <Input placeholder="Some text"/>
-                                <div>Successes</div>
-                                <Input successes={true} />
-                                <div>Error</div>
-                                <Input error={true} />
-                                <div>Error with message</div>
-                                <Input error={true} errorMessage="Something bag happen"/>
-                                <div>Disabled</div>
-                                <Input disabled />
-                                <div>With icon</div>
-                                <Input>
-                                    <div>: )</div>
-                                </Input>
-                            </form>
-                        </Route>
-                        {/* !not-delete! cli:route */}
-                    </Switch>
-                </div>
-            </Router>
-        </Provider>
-    </Suspense>
+  <Suspense fallback={LoadingState}>
+    <Provider store={store}>
+      <h2>Base app</h2>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Module</Link>
+            </li>
+            <li>
+              <Link to="/geocoder">geocoder</Link>
+            </li>
+            <li>
+              <Link to="/kit">UI Kit</Link>
+            </li>
+            {/* !not-delete! cli:link */}
+          </ul>
+          <hr />
+          <Switch>
+            <Route exact path="/">
+              <Module />
+            </Route>
+            <Route path="/geocoder">
+              <Geocoder />
+            </Route>
+            <Route path="/kit">
+              <form className="inputs">
+                <div>Default</div>
+                <Input placeholder="Some text" />
+                <div>Successes</div>
+                <Input successes={true} />
+                <div>Error</div>
+                <Input error={true} />
+                <div>Error with message</div>
+                <Input error={true} errorMessage="Something bag happen" />
+                <div>Disabled</div>
+                <Input disabled />
+                <div>With icon</div>
+                <Input>
+                  <div>: )</div>
+                </Input>
+              </form>
+            </Route>
+            {/* !not-delete! cli:route */}
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
+  </Suspense>
 );
 
 const container = document.getElementById('react-root');
---
+++ k2-packages/ui-kit/src/index.tsx
@@ -0,0 +1,5 @@
+import Input from '@k2-packages/ui-kit/src/input';
+
+export {
+  Input
+}

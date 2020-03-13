922746c48c8b659aa1d85a393ef8b1e1374e84ac
Ilya Kvachenko <ikvachenko@objectstyle.com>
2020-03-13 15:53:39 +0300

    env vars fix

--- k2-dev/base/webpack.config.js
+++ k2-dev/base/webpack.config.js
@@ -3,120 +3,128 @@ const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-console.log('MODE:', process.env.NODE_ENV);
-console.log('API:', process.env.API);
+// console.log('MODE:', process.env.NODE_ENV);
+// console.log('API:', process.env.API);
 
-module.exports = {
-  entry: {
-    main: './src/index.tsx',
-  },
-  mode: process.env.NODE_ENV,
-  output: {
-    filename: '[name].js',
-    chunkFilename: '[name].js',
-    path: path.join(__dirname, './dist'),
-  },
-  devtool: 'inline-source-map',
-  resolve: {
-    modules: [
-      'node_modules',
-    ],
-    extensions: ['.ts', '.tsx', '.js', '.json'],
-    symlinks: true,
-  },
-  devServer: {
-    port: 8080,
-    inline: true,
-    quiet: false,
-    noInfo: false,
-    stats: {
-      all: false,
-      wds: true,
-      assets: false,
-      colors: true,
-      version: false,
-      hash: false,
-      timings: false,
-      chunks: false,
-      chunkModules: false,
+module.exports = env => {
+  console.log('=== Webpack variables ===');
+  console.log('NODE_ENV: ', env.NODE_ENV);
+  console.log('API:', env.API);
+  console.log('=========================')
+
+  return {
+    entry: {
+      main: './src/index.tsx',
     },
-    historyApiFallback: true,
-    contentBase: path.resolve(__dirname, '/dist'),
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.API': process.env.API
-    }),
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(tsx?)|(ts?)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: require.resolve('babel-loader'),
-          options: {
-            rootMode: 'upward',
+    mode: process.env.NODE_ENV,
+    output: {
+      filename: '[name].js',
+      chunkFilename: '[name].js',
+      path: path.join(__dirname, './dist'),
+    },
+    devtool: 'inline-source-map',
+    resolve: {
+      modules: [
+        'node_modules',
+      ],
+      extensions: ['.ts', '.tsx', '.js', '.json'],
+      symlinks: true,
+    },
+    devServer: {
+      port: 8080,
+      inline: true,
+      quiet: false,
+      noInfo: false,
+      stats: {
+        all: false,
+        wds: true,
+        assets: false,
+        colors: true,
+        version: false,
+        hash: false,
+        timings: false,
+        chunks: false,
+        chunkModules: false,
+      },
+      historyApiFallback: true,
+      contentBase: path.resolve(__dirname, '/dist'),
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.API': JSON.stringify(env.API)
+      }),
+      new HtmlWebPackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(tsx?)|(ts?)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: require.resolve('babel-loader'),
+            options: {
+              rootMode: 'upward',
+            },
           },
         },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: { minimize: true },
-          },
-        ],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          'css-modules-typescript-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-              modules: {
-                localIdentName: '[local]-[hash:base64:5]',
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
+              options: { minimize: true },
+            },
+          ],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            'style-loader',
+            'css-modules-typescript-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+                modules: {
+                  localIdentName: '[local]-[hash:base64:5]',
+                },
               },
             },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.css$/i,
-        use: [
-          'style-loader',
-          'css-modules-typescript-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-              modules: {
-                localIdentName: '[local]-[hash:base64:5]',
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
               },
             },
-          },
-        ],
-      },
-    ],
-  },
-  optimization: {
-    splitChunks: {
-      name: true,
-      chunks: 'all',
+          ],
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            'style-loader',
+            'css-modules-typescript-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+                modules: {
+                  localIdentName: '[local]-[hash:base64:5]',
+                },
+              },
+            },
+          ],
+        },
+      ],
     },
-  },
+    optimization: {
+      splitChunks: {
+        name: true,
+        chunks: 'all',
+      },
+      minimize: false
+    },
+  };
 };

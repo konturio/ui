00d1fb0aa2e5cd256b102f7e8b3f8f9dcfb24a90
Ilya Kvachenko <ikvachenko@objectstyle.com>
2020-03-25 14:58:43 +0000

    build: minor fixes, revert peerDependencies

--- k2-packages/module/src/index.test.tsx
+++ k2-packages/module/src/index.test.tsx
@@ -1,3 +1,9 @@
-it('renders without crashing', () => {
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Module from './index';
 
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Module />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });

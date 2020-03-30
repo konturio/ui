eb30691f3f26a2dd7039e123fd0876a803656f3a
Alexandr Dubinin <akiyamka@gmail.com>
2020-03-30 11:17:54 +0300

    style: code style fix

--- k2-dev/base/src/modulesProvider.tsx
+++ k2-dev/base/src/modulesProvider.tsx
@@ -3,14 +3,14 @@ import Loadable from 'react-loadable';
 
 const loadedModulesCache = {};
 
-export function LazyModule({ path }) {
+export function LazyModule({ path }: { path: string }) {
   if (!loadedModulesCache[path]) {
     loadedModulesCache[path] = Loadable({
-      loader: () => import('@k2-packages/' + path),
-      loading: () => <div>Loading Module...</div>
+      loader: () => import(`@k2-packages/${path}`),
+      loading: () => <div>Loading Module...</div>,
     });
   }
 
   const LoadableModule = loadedModulesCache[path];
-  return <LoadableModule />
+  return <LoadableModule />;
 }

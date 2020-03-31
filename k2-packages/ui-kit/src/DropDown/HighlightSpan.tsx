e6a3b28c74ab7191f1eeaa8d3d6376332618a70b
Alexandr Dubinin <akiyamka@gmail.com>
2020-03-31 13:36:41 +0300

    feat: add dropdown as part of ui kit

---
+++ k2-packages/ui-kit/src/DropDown/HighlightSpan.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import style from './style.styl';
+
+const NEED_ESCAPE = ['[', '\\', '^', '$', '.', '|', '?', '*', '+', '(', ')'];
+function escapeSpecialCharacters(dangerString: string): string {
+  let safeString = '';
+  for (let i = 0; i < dangerString.length; i++) {
+    const char = dangerString.charAt(i);
+    if (NEED_ESCAPE.includes(char)) {
+      safeString += `\\${char}`;
+    } else {
+      safeString += char;
+    }
+  }
+  return safeString;
+}
+
+interface IHighlightSpan {
+  highlight: string;
+  children: string;
+}
+
+export default function HighlightSpan({ highlight, children }: IHighlightSpan) {
+  const escaped = escapeSpecialCharacters(highlight);
+  const between = children.split(new RegExp(escaped, 'gi'));
+  return (
+    <>
+      {between.map((t, i) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <span key={t + i}>
+          {t}
+          {i === between.length - 1 ? (
+            undefined
+          ) : <span className={style.highlight}>{highlight}</span>}
+        </span>
+      ))}
+    </>
+  );
+}
---
+++ k2-packages/ui-kit/src/DropDown/index.test.tsx
@@ -0,0 +1,9 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import DropDown from './index';
+
+it('renders without crashing', () => {
+  // const div = document.createElement('div');
+  // ReactDOM.render(<DropDown />, div);
+  // ReactDOM.unmountComponentAtNode(div);
+});

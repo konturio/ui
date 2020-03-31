ccb48fdf2fe7195e35a8d2d360357474715c88f7
Alexandr Dubinin <akiyamka@gmail.com>
2020-03-31 13:36:22 +0300

    feat: add isFocused prop in input

--- k2-packages/ui-kit/src/Input/index.tsx
+++ k2-packages/ui-kit/src/Input/index.tsx
@@ -1,4 +1,6 @@
-import React, { useState, forwardRef } from 'react';
+import React, {
+  useState, forwardRef, useEffect, useCallback,
+} from 'react';
 import clsx from 'clsx';
 import style from './style.styl';
 
@@ -9,6 +11,7 @@ export interface IInputProps extends React.HTMLProps<HTMLInputElement> {
   loading?: boolean;
   error?: boolean;
   message?: string;
+  isFocused?: boolean;
 }
 
 function createOnType(onType: IInputProps['onType']) {
@@ -27,10 +30,39 @@ function Input({
   onBlur,
   onType,
   disabled,
+  isFocused,
   ...props
 }: IInputProps, ref) {
   const onTypeHandler = createOnType(onType);
-  const [focus, setFocus] = useState(false);
+  const [focus, setFocus] = useState(isFocused);
+
+  useEffect(() => {
+    setFocus(isFocused);
+  }, [isFocused]);
+
+  useEffect(() => {
+    if (focus) {
+      ref.current.focus();
+    } else {
+      ref.current.blur();
+    }
+  }, [focus]);
+
+  const nativeFocusEventHandler = useCallback(e => {
+    e.preventDefault(); // Cancel default behavior and extend it
+    if (typeof onFocus === 'function') {
+      onFocus(e);
+    }
+    setFocus(true); // Delegate default behavior to effect
+  }, [onFocus]);
+
+  const nativeBlurEventHandler = useCallback(e => {
+    e.preventDefault(); // Cancel default behavior and extend it
+    if (typeof onBlur === 'function') {
+      onBlur(e);
+    }
+    setFocus(false); // Delegate default behavior to effect
+  }, [onBlur]);
 
   const dynamicClasses = {
     [style.successes]: successes,
@@ -46,8 +78,8 @@ function Input({
           {...props}
           ref={ref}
           onChange={e => ((onChange && onChange(e), onTypeHandler(e)))}
-          onFocus={e => ((onFocus && onFocus(e), setFocus(true)))}
-          onBlur={e => ((onBlur && onBlur(e), setFocus(false)))}
+          onFocus={nativeFocusEventHandler}
+          onBlur={nativeBlurEventHandler}
           disabled={disabled}
         />
         <div className={style.icons}>{children}</div>

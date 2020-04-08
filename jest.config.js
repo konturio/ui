893f7487f67e29631f15abacd5b63d0f7d60f4ba
Alexandr Dubinin <akiyamka@gmail.com>
2020-04-08 13:01:31 +0300

    feat: add support of react hooks in enzyme

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   automock: false,
+  // collectCoverage: true,
+  // coverageReporters: ["html"],
   setupFiles: [
-    './setupJest.js'
+    './jest.setup.js'
+  ],
+  setupFilesAfterEnv: [
+    './jest.setup.after-env.js'
   ],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/.jest/file-mock.js',
---
+++ jest.setup.after-env.js
@@ -0,0 +1,2 @@
+const enableHooks = require('jest-react-hooks-shallow').default;
+ enableHooks(jest);

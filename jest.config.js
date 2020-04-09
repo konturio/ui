554223ca7ef36f37ffad0fbff71fd2ade85cfd52
Alexandr Dubinin <akiyamka@gmail.com>
2020-04-09 11:34:28 +0300

    chore: move setup files in jest folder

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,10 @@
 module.exports = {
   automock: false,
-  // collectCoverage: true,
-  // coverageReporters: ["html"],
   setupFiles: [
-    './jest.setup.js'
+    './.jest/jest.setup.js'
   ],
   setupFilesAfterEnv: [
-    './jest.setup.after-env.js'
+    './.jest/jest.setup.after-env.js'
   ],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/.jest/file-mock.js',
@@ -15,22 +13,10 @@ module.exports = {
   'moduleDirectories': [
     'node_modules',
   ],
-  // 'projects': [
-  //   '<rootDir>/k2-packages/',
-  //   '<rootDir>/k2-dev/'
-  // ],
   verbose: true,
   transform: {
     '.(ts|tsx)': 'ts-jest',
     '.+\\.(css|styl)$': 'jest-css-modules-transform'
   },
-  // 'testRegex': '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
-  // 'moduleFileExtensions': [
-  //   'ts',
-  //   'tsx',
-  //   'js',
-  //   'jsx',
-  //   'json'
-  // ],
   testPathIgnorePatterns: ['/tsclib/'],
 };

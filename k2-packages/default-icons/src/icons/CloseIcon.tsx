8b010416dcc51cae1e25ea708a991be46b47930c
Alexandr Dubinin <akiyamka@gmail.com>
2020-05-19 11:08:00 +0300

    better icons in geocoder search bar

--- k2-packages/default-icons/src/icons/CloseIcon.tsx
+++ k2-packages/default-icons/src/icons/CloseIcon.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 
 const CloseIcon = React.memo((props: React.SVGProps<SVGSVGElement>) => (
-  <svg width="14" height="14" viewBox="0 0 14 14" {...props}>
-    <path
-      d="M13.9844 1.42188L8.40625 7L13.9844 12.5781L12.5781 13.9844L7 8.40625L1.42188 13.9844L0.015625 12.5781L5.59375 7L0.015625 1.42188L1.42188 0.015625L7 5.59375L12.5781 0.015625L13.9844 1.42188Z"
-      fill="currentColor"
-    />
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M6 6L18 18M6 18L18 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
   </svg>
 ));
 

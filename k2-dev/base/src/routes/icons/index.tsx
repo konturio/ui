3ff36398050dad244eb8db8dbf2c9fec21856755
Alexandr Dubinin <akiyamka@gmail.com>
2020-05-26 11:25:05 +0300

    refactor base app routes

---
+++ k2-dev/base/src/routes/icons/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SearchIcon, CallIcon, CloseIcon, FlameIcon, FireTruckIcon, HydrantIcon } from '@k2-packages/default-icons';
+
+export default function Icons(): JSX.Element {
+  return (
+    <>
+      <SearchIcon className="hello" />
+      <CallIcon />
+      <CloseIcon />
+      <FlameIcon />
+      <FireTruckIcon />
+      <HydrantIcon />
+    </>
+  );
+}

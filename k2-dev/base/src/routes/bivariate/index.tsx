5104e4e10d6925f46e33d4d662dfb56c7bdc8bf8
Andrei Kucherov <kucherov.andrei@gmail.com>
2021-03-01 12:44:53 +0300

    chore(axis-control): Fix build

--- k2-dev/base/src/routes/bivariate/index.tsx
+++ k2-dev/base/src/routes/bivariate/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { Selector, Legend, AxisControl, Rotator } from '@k2-packages/ui-kit';
+import { Selector, AxisControl, Rotator } from '@k2-packages/ui-kit';
 import style from './style.styl';
 import {
   Stat,
@@ -178,17 +178,6 @@ export default function Bivariate(): JSX.Element {
               onSelectDenominator={(horisontal: boolean, index: number, denId: string) => {
                 console.log('on select denominator');
               }}
-              legend={
-                <Legend
-                  size={3}
-                  angle={-45}
-                  axis={selectedAxis}
-                  cells={new Array(9).fill(0).map((c, i) => ({
-                    color: `hsl(${(360 / 9) * i}, 50%, 50%)`,
-                    label: String(i),
-                  }))}
-                />
-              }
             />
           </Rotator>
         )}

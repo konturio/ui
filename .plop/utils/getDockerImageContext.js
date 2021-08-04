0d82129c676f65781e6feb42b41b84752566797b
Alexandr Dubinin <adubinin@kontur.io>
2021-08-04 18:01:06 +0300

    remove extra files

--- .plop/utils/getDockerImageContext.js
+++
@@ -1,9 +0,0 @@
-module.exports.getDockerImageContext = function (imageName) {
-  return (...commands) => {
-    if (commands.length > 1) {
-      return commands.map(command => `docker exec -t ${imageName} bash -c "${command}"`);
-    } else {
-      return `docker exec -t ${imageName} bash -c "${commands[0]}"`;
-    }
-  }
-}

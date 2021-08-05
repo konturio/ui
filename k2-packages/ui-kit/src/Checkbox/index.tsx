11487acbcacb5883d53f03c16b7517a2a094d0c4
Alexandr Dubinin <adubinin@kontur.io>
2021-08-05 23:17:16 +0300

    fix: radio in checkbox broken layout in chrome

--- k2-packages/ui-kit/src/Checkbox/index.tsx
+++ k2-packages/ui-kit/src/Checkbox/index.tsx
@@ -22,7 +22,7 @@ export function Checkbox({
       <input id={id} name={name} type="checkbox" className={`${s.checkbox} ${className}`} {...native} />
       <div className={s.icon}>
         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M3 9L6.5 12.5L14 5" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M3 9L6.5 12.5L14 5" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
         </svg>
       </div>
       <span className={s.label}>{label}</span>

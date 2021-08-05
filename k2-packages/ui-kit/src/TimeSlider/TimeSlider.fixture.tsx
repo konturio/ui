824eb3079d99343456827f37a00bb9ecb6d890bc
Alexandr Dubinin <adubinin@kontur.io>
2021-08-05 20:04:22 +0300

    feat: fixature for slider

---
+++ k2-packages/ui-kit/src/TimeSlider/TimeSlider.fixture.tsx
@@ -0,0 +1,31 @@
+import { useValue } from 'react-cosmos/fixture';
+import { TimeSlider } from '.';
+
+export default {
+  TimeSlider: <div style={{ padding: 'var(--double-unit)' }}><TimeSlider
+    rulerNumbers={[5, 10, 15, 20, 25, 30]}
+    sliderValue={20}
+    onSliderChange={(val) => {}}
+    colorsLegend={['#5433FF', '#3683ff', '#20BDFF', '#5fdce6', '#A5FECB']}
+  /></div>,
+  Interactive: () => {
+    const [state, setState] = useValue('value', { defaultValue: 15 } as { defaultValue: number });
+    const [steps, setSteps] = useValue('steps', { defaultValue: 6 } as { defaultValue: number });
+    return (
+      <div style={{ padding: 'var(--double-unit)' }}>
+        <div style={{ whiteSpace: 'pre', padding: '1em' }}>
+          <div>
+            Value: {state}
+          </div>
+        </div>
+
+        <TimeSlider
+          rulerNumbers={Array.from(new Array(steps)).map((_, i) => i * 5)}
+          sliderValue={state}
+          onSliderChange={(val) => setState(val)}
+          colorsLegend={['#5433FF', '#3683ff', '#20BDFF', '#5fdce6', '#A5FECB']}
+        />
+      </div>
+    );
+  },
+};

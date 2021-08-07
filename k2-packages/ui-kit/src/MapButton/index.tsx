0bf967b5f2b810d1869a7a60a7b14baaa630b3d1
Alexandr Dubinin <adubinin@kontur.io>
2021-08-07 15:17:49 +0300

    feat: map button

---
+++ k2-packages/ui-kit/src/MapButton/index.tsx
@@ -0,0 +1,12 @@
+import s from './style.module.css';
+
+export interface MapButton {
+  onClick?: React.MouseEventHandler<HTMLButtonElement>,
+  disabled?: boolean;
+}
+
+export function MapButton({ onClick, children, disabled }: React.PropsWithChildren<MapButton>) {
+  return <button className={s.button} onClick={onClick} disabled={disabled}>
+    { children }
+  </button>
+}

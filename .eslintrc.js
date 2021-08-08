b82c20450a7f7a6149090d0a2006e92520cc2476
Alexandr Dubinin <adubinin@kontur.io>
2021-08-08 18:32:30 +0300

    fix: eslint broken

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   parserOptions: {
-    project: ['tsconfig.json'],
     ecmaVersion: 2020,
     sourceType: 'module',
     ecmaFeatures: {

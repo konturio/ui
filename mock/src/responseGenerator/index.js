3c825b7ffe912eda8c1ac48fdd1e9d98fb7ae6fa
Alexandr Dubinin <adubinin@kontur.io>
2021-08-13 18:13:15 +0200

    chore: remove unused mock folder

--- mock/src/responseGenerator/index.js
+++
@@ -1,7 +0,0 @@
-import injectVariables from './injectVariables';
-import readJSON from './readJSON';
-
-export default async function generateResponse({ path: pathToTemplate, variables }) {
-  const mockTemplate = await readJSON(pathToTemplate);
-  return injectVariables({ template: mockTemplate, variables })
-}
--- mock/src/responseGenerator/injectVariables.js
+++
@@ -1,7 +0,0 @@
-export default function injectVariables({ template, variables }) {
-  Object.keys(variables).forEach(variable => {
-    const regExp = new RegExp(variable, 'gm')
-    template = template.replace(regExp, variables[variable]);
-  });
-  return template; 
-}

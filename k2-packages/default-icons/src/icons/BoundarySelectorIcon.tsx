a806b4657b932d195f434d1191c02ccf20042aef
Andrei Kucherov <kucherov.andrei@gmail.com>
2021-11-05 11:57:39 +0300

    feat(default-icons): Add draw-tools icon:

--- k2-packages/default-icons/src/icons/BoundarySelectorIcon.tsx
+++ k2-packages/default-icons/src/icons/BoundarySelectorIcon.tsx
@@ -1,12 +1,16 @@
 import { memo } from 'react';
 
 const BoundarySelectorIcon = memo((props: React.SVGProps<SVGSVGElement>) => (
-  <svg width="20" height="22" viewBox="0 0 20 22" fill="none">
+  <svg width="18" height="21" viewBox="0 0 18 21" fill="none">
+    <path d="M4.98737 4.07847L3.22876 1.03247" stroke="white" strokeWidth="1.3" />
+    <path d="M7.21138 3.78567L8.1217 0.388306" stroke="white" strokeWidth="1.3" />
+    <path d="M8.99111 5.15131L12.0371 3.3927" stroke="white" strokeWidth="1.3" />
+    <path d="M0.868433 9.84088L3.91443 8.08228" stroke="white" strokeWidth="1.3" />
+    <path d="M0.224216 4.94783L3.62158 5.85815" stroke="white" strokeWidth="1.3" />
     <path
-      d="M11.8099 7.75L1.19006 1.25L2.30994 19.25L7.80994 18.25L15.8099 20.75L18.8099 6.25L11.8099 7.75Z"
+      d="M7.04717 7.60757L16.3557 14.9121L11.6493 15.5786L8.71884 19.3212L7.04717 7.60757Z"
       stroke="white"
       strokeWidth="1.3"
-      strokeLinejoin="round"
     />
   </svg>
 ));

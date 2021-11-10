22877f50b43f1260ce3b4cc3fceb5d0369cfb7dd
Andrei Kucherov <kucherov.andrei@gmail.com>
2021-11-10 11:15:15 +0300

    feat(ui-components): Add ability to provide additional styling for tabs and panel

--- k2-packages/ui-kit/src/Tabs/index.tsx
+++ k2-packages/ui-kit/src/Tabs/index.tsx
@@ -1,16 +1,19 @@
-import { Children, useMemo, isValidElement, useCallback } from 'react';
+import { Children, isValidElement, ReactNode, useCallback, useMemo } from 'react';
 import cn from 'clsx';
 import s from './style.module.css';
 
-export function Tabs({
-  children,
-  current: currentTabId,
-  onTabChange: setCurrentTabId,
-}: {
+export interface TabsProps {
   children: JSX.Element[];
   current: string;
   onTabChange: (tabId: string) => void;
-}) {
+  classes?: {
+    tabBtn?: string;
+    tabName?: string;
+    tabBody?: string;
+  };
+}
+
+export function Tabs({ children, current: currentTabId, onTabChange: setCurrentTabId, classes }: TabsProps) {
   const tabSelectHandler = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => setCurrentTabId(e.target.value),
     [setCurrentTabId],
@@ -38,17 +41,23 @@ export function Tabs({
               className={s.input}
               id={c.id}
             />
-            <label htmlFor={c.id} className={cn(s.tabBtn, { [s.current]: c.id === currentTabId })}>
-              <span className={s.name}>{c.name}</span>
+            <label htmlFor={c.id} className={cn(s.tabBtn, classes?.tabBtn, { [s.current]: c.id === currentTabId })}>
+              <span className={cn(s.name, classes?.tabName)}>{c.name}</span>
             </label>
           </>
         ))}
       </div>
-      <div className={s.body}>{currentTab ? currentTab.element : null}</div>
+      <div className={cn(s.body, classes?.tabBody)}>{currentTab ? currentTab.element : null}</div>
     </div>
   );
 }
 
-export function Tab({ children, id, name }: React.PropsWithChildren<{ id: string; name: string }>) {
+export interface TabProps {
+  children: ReactNode;
+  id: string;
+  name: string;
+}
+
+export function Tab({ children, id, name }: TabProps) {
   return <div className={s.tab}>{children}</div>;
 }

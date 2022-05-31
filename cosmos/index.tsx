29d90e35d60a13979ff7e27174a7dc1532cde670
Pavel Ropakov <propakov@kontur.io>
2022-05-31 10:42:11 +0300

    defaultIconsVersion added to cosmos log

--- cosmos/index.tsx
+++ cosmos/index.tsx
@@ -3,6 +3,7 @@ import { decorators, fixtures, rendererConfig } from './cosmos.userdeps';
 
 import { version as defaultThemeVersion } from './../k2-packages/default-theme/package.json';
 import { version as uiKitVersion } from './../k2-packages/ui-kit/package.json';
+import { version as defaultIconsVersion } from './../k2-packages/default-icons/package.json';
 
 mountDomRenderer({ rendererConfig, decorators, fixtures });
 
@@ -11,4 +12,5 @@ if (import.meta.hot) import.meta.hot!.accept();
 console.table({
   'default-theme': defaultThemeVersion,
   'ui-kit': uiKitVersion,
+  'default-icons': defaultIconsVersion,
 });

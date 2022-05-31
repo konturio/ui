f3fcb7ba677fc6ea5c03a36fd2b30e8037e8c8a4
Pavel Ropakov <propakov@kontur.io>
2022-05-31 10:33:49 +0300

    update cosmos file imports

--- cosmos/index.tsx
+++ cosmos/index.tsx
@@ -3,9 +3,6 @@ import { decorators, fixtures, rendererConfig } from './cosmos.userdeps';
 
 import { version as defaultThemeVersion } from './../k2-packages/default-theme/package.json';
 import { version as uiKitVersion } from './../k2-packages/ui-kit/package.json';
-import { version as iconsVersion } from './../k2-packages/icons/package.json';
-import { version as mapDrawToolsVersion } from './../k2-packages/map-draw-tools/package.json';
-import { version as mapVersion } from './../k2-packages/map/package.json';
 
 mountDomRenderer({ rendererConfig, decorators, fixtures });
 
@@ -14,7 +11,4 @@ if (import.meta.hot) import.meta.hot!.accept();
 console.table({
   'default-theme': defaultThemeVersion,
   'ui-kit': uiKitVersion,
-  icons: iconsVersion,
-  'map-draw-tools': mapDrawToolsVersion,
-  map: mapVersion,
 });

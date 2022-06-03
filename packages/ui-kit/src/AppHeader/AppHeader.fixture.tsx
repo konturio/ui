b26613021bb17da07bba9b0b13f48b595a6b67e1
Pavel Ropakov <propakov@kontur.io>
2022-06-03 16:43:27 +0300

    test surge

--- packages/ui-kit/src/AppHeader/AppHeader.fixture.tsx
+++ packages/ui-kit/src/AppHeader/AppHeader.fixture.tsx
@@ -2,6 +2,6 @@ import { AppHeader } from '.';
 
 export default (
   <div style={{ flex: '1' }}>
-    <AppHeader title="Example" onChatClick={console.log} installChat={console.log} />
+    <AppHeader title="Test123" onChatClick={console.log} installChat={console.log} />
   </div>
 );

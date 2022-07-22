1f38b6d59410707d8c69232052a4c07404e43664
Andrei Kucherov <kucherov.andrei@gmail.com>
2022-07-22 19:43:44 +0300

    fix(ui-kit): add more fixtures to select

--- packages/ui-kit/src/Select/components/index.ts
+++ packages/ui-kit/src/Select/components/index.ts
@@ -1,1 +1,2 @@
 export * from './SelectButton';
+export * from './SelectItem';
--- packages/ui-kit/src/Select/fixtures/Select.fixture.tsx
+++ packages/ui-kit/src/Select/fixtures/Select.fixture.tsx
@@ -1,12 +1,12 @@
 import { Select } from '../index';
-import { SelectItem } from '../types';
+import { SelectItemType } from '../types';
 
-const items: SelectItem[] = [
+const items: SelectItemType[] = [
   { title: 'Multiple Choice Questions', value: 1 },
   { title: 'True and False Statements', value: 2 },
   { title: 'Pair Matching', value: 3 },
-  { title: 'Essay Writing', value: 4 },
-  { title: 'Oral Questions', value: 5 },
+  { title: 'Essay Writing', value: 4, hasDivider: true },
+  { title: 'Oral Questions', value: 5, disabled: true },
   { title: 'Short questions and answers', value: 6 },
   { title: 'Computational Questions', value: 7 },
 ];
@@ -17,7 +17,22 @@ function itemToString(item) {
 
 export default {
   Base: (
-    <Select label="Select Label" items={items} itemToString={itemToString}>
+    <Select label="Select Base" items={items} itemToString={itemToString}>
+      Select Example
+    </Select>
+  ),
+  Disabled: (
+    <Select label="Select Disabled" items={items} itemToString={itemToString} disabled>
+      Select Example
+    </Select>
+  ),
+  WithError: (
+    <Select label="Select With Error" items={items} itemToString={itemToString} error="Error messaqge">
+      Select Example
+    </Select>
+  ),
+  Inline: (
+    <Select label="Inline Select" items={items} type="inline" itemToString={itemToString}>
       Select Example
     </Select>
   ),

13d6736f6f4008c1fbafb66d25130a0ec03d3191
Andrei Kucherov <kucherov.andrei@gmail.com>
2022-08-18 15:13:35 +0300

    chore: default export fixes

--- packages/ui-kit/src/index.ts
+++ packages/ui-kit/src/index.ts
@@ -29,6 +29,7 @@ export * from './Dropdown';
 export * from './Dropdown/components';
 export * from './MenuButton';
 export * from './MenuButton/components';
+export * from './Combobox/components';
 export * from './Select';
 export * from './Select/components';
 export * from './Autocomplete';

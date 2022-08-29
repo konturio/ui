f5475b268394fac3aff6277d73093e11984a13fa
Alexandr Dubinin <akiyamka@gmail.com>
2022-08-29 20:05:33 +0200

    selected cluster highlighting

--- packages/ui-kit/src/Timeline/Timeline.tsx
+++ packages/ui-kit/src/Timeline/Timeline.tsx
@@ -30,6 +30,24 @@ interface OnSelectPayload {
   items: TimelineEntry['id'][];
 }
 
+// TODO: Contribute it to timeline-vis package
+interface OnClickPayload {
+  event: PointerEvent;
+  customTime: null;
+  group: null;
+  isCluster: boolean;
+  /* Cluster that will be selected */
+  item: null | string;
+  /* Items that was selected */
+  items: null | TimelineEntry['id'][];
+  pageX: number;
+  pageY: number;
+  time: Date;
+  what: 'background' | 'item';
+  x: number;
+  y: number;
+}
+
 const createDatasetItem = (e: TimelineEntry): DataItem => ({
   id: e.id,
   start: e.start,
@@ -89,7 +107,18 @@ export function Timeline({ dataset, selected, ...rest }: TimelineProps): JSX.Ele
   // Add selection
   useEffect(() => {
     if (timeline === null) return;
-    timeline.setSelection(selected);
+    /* Find out what cluster should be highlighted */
+    // TODO - add itemSet in public interface if lib
+    // TODO - performance can be improved here by using more clever algorithm
+    // @ts-expect-error timeline not expose itemSet in interface.
+    const affectedClusters = timeline.itemSet.clusters.reduce((acc, cluster) => {
+      const clusterEntries = cluster.getData().items.map((i) => i.id);
+      if (selected.some((s) => clusterEntries.includes(s))) {
+        acc.push(cluster.id);
+      }
+      return acc;
+    }, []);
+    timeline.setSelection([selected, ...affectedClusters]);
   }, [timeline, selected]);
 
   // Add on select handler
@@ -99,30 +128,25 @@ export function Timeline({ dataset, selected, ...rest }: TimelineProps): JSX.Ele
     if (!onSelect) return;
     if (timeline === null) return;
     if (!dataMapRef.current) return;
-    const onSelectCb = ({ items, event }: OnSelectPayload) => {
-      const selectedEntries = items.reduce((acc, id) => {
-        const entry = dataMapRef.current.get(id);
-        if (entry) acc.push(entry);
-        else {
-          // look like it's cluster
-          try {
-            // TODO - add itemSet in public interface if lib
-            // @ts-expect-error timeline not expose itemSet in interface.
-            const cluster = timeline.itemSet.clusters.find((c) => c.id === id);
-            const entriesInCluster = cluster.getData().items;
-            return acc.concat(entriesInCluster);
-          } catch (e) {
-            console.error('Cluster not found');
-          }
-        }
-        return acc;
-      }, [] as TimelineEntry[]);
-      onSelect(selectedEntries);
+    const onSelectCb = (payload: OnClickPayload) => {
+      if (payload.isCluster) {
+        // TODO - add itemSet in public interface if lib
+        // @ts-expect-error timeline not expose itemSet in interface.
+        const cluster = timeline.itemSet.clusters.find((c) => c.id === payload.item);
+        const entriesInCluster = cluster.getData().items.map((e) => dataMapRef.current.get(e.id));
+        onSelect(entriesInCluster);
+      } else {
+        const selectedEntries = timeline.getSelection().reduce((acc, id) => {
+          const entry = dataMapRef.current.get(id);
+          if (entry) acc.push(entry);
+          return acc;
+        }, [] as TimelineEntry[]);
+        onSelect(selectedEntries);
+      }
     };
-
-    timeline.on('select', onSelectCb);
+    timeline.on('click', onSelectCb);
     return () => {
-      timeline.off('select', onSelectCb);
+      timeline.off('click', onSelectCb);
     };
     // I use data from ref, because data changes will handled by timeline instance change
   }, [timeline, onSelect]);

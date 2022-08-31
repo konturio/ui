fc38ffe60e81f8c749e34e9d81681bddc4920476
Andrei Kucherov <kucherov.andrei@gmail.com>
2022-08-31 12:26:11 +0300

    fix(ui-kit): small updates for radiobutton

--- packages/ui-kit/src/Radio/index.tsx
+++ packages/ui-kit/src/Radio/index.tsx
@@ -1,27 +1,54 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, forwardRef, useCallback } from 'react';
 import { LineItem } from '../LineItem';
-import s from './style.module.css';
+import style from './style.module.css';
+import cn from 'clsx';
+import { ForwardRefComponent } from '../utils/component-helpers/polymorphic';
 
-interface Radio {
+interface RadioProps {
   id: string;
   name?: string;
   label?: React.ReactChild | React.ReactChild[];
   className?: string;
-  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
   block?: boolean;
+  onChange?: (isChecked: boolean) => void;
+  classes?: {
+    input?: string;
+    disabled?: string;
+  };
 }
 
-export function Radio({
-  id,
-  name,
-  label,
-  className = '',
-  block = true,
-  ...native
-}: Radio & React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>): React.ReactElement {
-  return (
-    <LineItem id={id} label={label} className={className} block={block} cursor="pointer">
-      <input id={id} name={name} type="radio" className={s.radio} {...native} />
-    </LineItem>
-  );
-}
+export const Radio = forwardRef(
+  ({ id, name, label, className = '', block = true, onChange, classes, disabled, ...nativeProps }, ref) => {
+    const onRadioChange = useCallback(
+      (e: ChangeEvent<HTMLInputElement>) => {
+        if (onChange && typeof onChange === 'function') {
+          onChange(e.target.checked);
+        }
+      },
+      [onChange],
+    );
+
+    return (
+      <LineItem
+        id={id}
+        label={label}
+        className={cn(className, disabled && classes?.disabled)}
+        block={block}
+        cursor="pointer"
+      >
+        <input
+          id={id}
+          name={name}
+          type="radio"
+          className={cn(style.radio, classes?.input)}
+          onChange={onRadioChange}
+          disabled={disabled}
+          ref={ref}
+          {...nativeProps}
+        />
+      </LineItem>
+    );
+  },
+) as ForwardRefComponent<'input', RadioProps>;
+
+Radio.displayName = 'Radio';

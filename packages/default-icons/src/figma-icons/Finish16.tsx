e45cf5f2a725572800e2972287ae5fbc822f4605
Pavel Ropakov <propakov@kontur.io>
2022-09-08 15:04:55 +0300

    feat(icons) new icons exported

--- packages/default-icons/src/figma-icons/Finish16.tsx
+++ packages/default-icons/src/figma-icons/Finish16.tsx
@@ -3,7 +3,7 @@ import { SVGProps, memo } from 'react';
 const Finish16 = (props: SVGProps<SVGSVGElement>) => (
   <svg width={16} height={16} viewBox="0 0 16 16" fill="none" {...props}>
     <path
-      d="M4 8.22222L6.9697 11.3333L13.3333 4.66666"
+      d="M4 8.22223L6.9697 11.3333L13.3333 4.66667"
       stroke="currentColor"
       strokeWidth={1.3}
       strokeLinecap="square"
---
+++ packages/default-icons/src/figma-icons/PlayOutline24.tsx
@@ -0,0 +1,28 @@
+import { SVGProps, memo } from 'react';
+
+const PlayOutline24 = (props: SVGProps<SVGSVGElement>) => (
+  <svg width={24} height={24} viewBox="0 0 24 24" fill="none" {...props}>
+    <path
+      d="M9 9.45221C9 9.10875 9.39914 8.89102 9.72535 9.05654L14.7464 11.6043C15.0845 11.7759 15.0845 12.2241 14.7464 12.3957L9.72535 14.9435C9.39914 15.109 9 14.8913 9 14.5478V9.45221Z"
+      stroke="currentColor"
+      strokeWidth={1.3}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <rect
+      x={1}
+      y={4}
+      width={22}
+      height={16}
+      rx={1.5}
+      stroke="currentColor"
+      strokeWidth={1.3}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+PlayOutline24.displayName = 'PlayOutline24';
+const Memo = memo(PlayOutline24);
+export default Memo;

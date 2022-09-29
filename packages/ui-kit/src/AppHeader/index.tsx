1f7fd4e74f645e6e857a724278b29dce19b28a64
VK <vkozel@kontur.io>
2022-09-29 13:19:02 +0300

    fix(app-header): remove irrelevant chat props

--- packages/ui-kit/src/AppHeader/index.tsx
+++ packages/ui-kit/src/AppHeader/index.tsx
@@ -1,30 +1,17 @@
-import type { ReactNode} from 'react';
-import { useEffect } from 'react';
+import type { ReactNode } from 'react';
 import { Logo } from '../Logo';
 import s from './style.module.css';
-
-const CHAT_BTN_ID = 'kontur_header_chat_btn';
 
 export function AppHeader({
   title,
   children,
-  onChatClick,
-  installChat,
   logo = <Logo height={24} />,
   afterChatContent,
 }: React.PropsWithChildren<{
   title: string | JSX.Element;
   logo?: JSX.Element;
-  onChatClick?: React.MouseEventHandler<HTMLButtonElement>;
-  installChat?: (id: string) => void;
   afterChatContent?: ReactNode;
 }>) {
-  useEffect(() => {
-    if (installChat) {
-      installChat(CHAT_BTN_ID);
-    }
-  }, [installChat]);
-
   return (
     <div className={s.appHeader}>
       <div className={s.logo}>{logo}</div>

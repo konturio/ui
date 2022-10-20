eb04d19943acb249b3a2dbe7aadb02cda982a221
IlyaIzr <50058726+IlyaIzr@users.noreply.github.com>
2022-10-20 13:54:37 +0300

    feat(textarea): 9250 - add Textarea component (#35)
    * add textarea component based on input component
    
    * add export change cosmos file
    
    * add endline
    
    * add textarea fixture example
    
    * chore: clean before build
    
    * rollback cosmos.userdeps.js
    
    Co-authored-by: Alexandr Dubinin <akiyamka@gmail.com>

---
+++ packages/ui-kit/src/Textarea/Textarea.fixture.tsx
@@ -0,0 +1,100 @@
+import { Textarea } from '.';
+import { InfoOutline16, Search16 } from '@konturio/default-icons';
+import type { ChangeEvent } from 'react';
+import { useEffect, useState } from 'react';
+
+const WrappedTextarea = ({ value, ...props }) => {
+  const [innerVal, setInnerVal] = useState(value || '');
+  useEffect(() => {
+    setInnerVal(value);
+  }, [value]);
+  return <Textarea {...props} value={innerVal} onChange={(e) => setInnerVal(e.target.value)} />;
+};
+
+const getInputStates = (props: {
+  error?: boolean | string;
+  disabled?: boolean;
+  children?: React.ReactNode;
+  isFocused?: boolean;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}) => (
+  <div style={{ display: 'flex', flexFlow: 'column nowrap', gap: '1em' }}>
+    <>
+      Empty: <WrappedTextarea value="" placeholder="" {...props} />
+    </>
+    <>
+      Placeholder: <WrappedTextarea placeholder="Placeholder" value="" {...props} />
+    </>
+    <>
+      Long placeholder: <WrappedTextarea placeholder="Looooong long long long long placeholder" value="" {...props} />
+    </>
+    <>
+      Placeholder on top: <WrappedTextarea value="test" placeholder="Placeholder" showTopPlaceholder {...props} />
+    </>
+    <>
+      Long placeholder on top:
+      <WrappedTextarea value="test" placeholder="Looooong long long long long placeholder" showTopPlaceholder {...props} />
+    </>
+    <>
+      Placeholder + Value
+      <WrappedTextarea value="test" placeholder="Placeholder" {...props} />
+    </>
+
+    <>
+      Width from 100px to 300px, height from 60px to 100px, 
+      <WrappedTextarea
+        minWidth='100px' minHeight='60px'
+        maxWidth='300px' maxHeight='100px'
+        value="Max Width 300px, Max height 100px" placeholder="Placeholder" {...props}
+      />
+    </>
+    <>
+      Long Placeholder + Long Value
+      <WrappedTextarea
+        value="Looooong long long long long value"
+        placeholder="Looooong long long long long placeholder"
+        {...props}
+      />
+    </>
+    <>
+      With message
+      <WrappedTextarea value="Value" message="With message" {...props} />
+    </>
+    <>
+      With label
+      <WrappedTextarea
+        value="my biography"
+        message="Bio"
+        {...props}
+        renderLabel={
+          <div style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
+            Label <InfoOutline16 style={{ color: 'var(--faint-strong)' }} />
+          </div>
+        }
+      />
+    </>
+    <>
+      With label + Top placeholder
+      <WrappedTextarea
+        value="my biography"
+        message="Bio"
+        showTopPlaceholder
+        placeholder="Share biography"
+        {...props}
+        renderLabel={
+          <div style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
+            Label <InfoOutline16 style={{ color: 'var(--faint-strong)' }} />
+          </div>
+        }
+      />
+    </>
+  </div>
+);
+
+export default {
+  Base: getInputStates({ onChange: console.log, isFocused: false }),
+  'Error - String': getInputStates({ onChange: console.log, isFocused: false, error: 'This is test error message!' }),
+  'Error - Boolean': getInputStates({ onChange: console.log, isFocused: false, error: true }),
+  Disabled: getInputStates({ onChange: console.log, isFocused: false, disabled: true }),
+  'With icon': getInputStates({ onChange: console.log, isFocused: false, children: <Search16 /> }),
+};
---
+++ packages/ui-kit/src/Textarea/index.tsx
@@ -0,0 +1,156 @@
+import React, { forwardRef, useCallback, useEffect, useImperativeHandle, useRef, useState } from 'react';
+import cn from 'clsx';
+import s from './style.module.css';
+
+export interface TextareaProps extends React.HTMLProps<HTMLTextAreaElement> {
+  onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  className?: string;
+  error?: boolean | string;
+  message?: string;
+  isFocused?: boolean;
+  showTopPlaceholder?: boolean;
+  renderLabel?: JSX.Element | string;
+  classes?: {
+    inputBox?: string;
+    input?: string;
+    topPlaceholder?: string;
+    placeholder?: string;
+    label?: string;
+    error?: string;
+  };
+  maxWidth?: string;
+  minWidth?: string;
+  maxHeight?: string;
+  minHeight?: string;
+}
+
+function preventDefault(e: { preventDefault?: () => void }): void {
+  e.preventDefault && e.preventDefault();
+}
+
+function isFunc<T>(maybeFn: unknown): maybeFn is (T) => void {
+  return typeof maybeFn === 'function';
+}
+
+function TextareaComponent(
+  {
+    className,
+    error,
+    children,
+    message,
+    renderLabel,
+    onChange,
+    onFocus,
+    onBlur,
+    disabled,
+    isFocused,
+    classes,
+    showTopPlaceholder = false,
+    placeholder,
+    value,
+    maxWidth,
+    minWidth,
+    maxHeight,
+    minHeight,
+    ...props
+  }: TextareaProps,
+  ref,
+): JSX.Element {
+  const [focus, setFocus] = useState(isFocused);
+  const textareaStyles = { maxWidth, minWidth, maxHeight, minHeight }
+
+  useEffect(() => {
+    setFocus(isFocused);
+  }, [isFocused]);
+
+  useEffect(() => {
+    if (ref?.current) {
+      if (focus) {
+        isFunc(ref.current.focus) && ref.current.focus();
+      } else {
+        isFunc(ref.current.blur) && ref.current.blur();
+      }
+    }
+  }, [focus, ref]);
+
+  const nativeFocusEventHandler = useCallback(
+    (e) => {
+      preventDefault(e);
+      isFunc<typeof e>(onFocus) && onFocus(e);
+      setFocus(true); // Delegate default behavior to effect
+    },
+    [onFocus],
+  );
+
+  const nativeBlurEventHandler = useCallback(
+    (e) => {
+      preventDefault(e);
+      isFunc<typeof e>(onBlur) && onBlur(e);
+      setFocus(false); // Delegate default behavior to effect
+    },
+    [onBlur],
+  );
+
+  const dynamicClasses = {
+    [s.error]: !!error,
+    [s.focus]: focus,
+    [s.disabled]: disabled,
+    [s.topPlaceholderValue]: showTopPlaceholder && value,
+    [s.topPlaceholderWrapper]: showTopPlaceholder,
+  };
+
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+  useImperativeHandle(ref, () => ({
+    focus: (): void => inputRef.current?.focus(),
+    blur: (): void => inputRef.current?.blur(),
+    selectText: (): void => inputRef.current?.select(),
+  }));
+
+  const onInputChange = useCallback(
+    (ev: React.ChangeEvent<HTMLTextAreaElement>) => {
+      onChange && onChange(ev);
+    },
+    [onChange],
+  );
+
+  const getPlaceholder = () => {
+    if (!placeholder) return;
+
+    if (!showTopPlaceholder) {
+      if (value) return;
+      return <div className={cn(s.placeholder, classes?.placeholder)}>{placeholder}</div>;
+    }
+
+    return (
+      <div className={cn(s.placeholder, classes?.topPlaceholder, value && showTopPlaceholder && s.topPlaceholder)}>
+        {placeholder}
+      </div>
+    );
+  };
+
+  return (
+    <div className={cn(s.root, className, dynamicClasses)}>
+      {renderLabel && <div className={cn(s.label, classes?.label)}>{renderLabel}</div>}
+      <div className={cn(s.inputBox, classes?.inputBox)}>
+        {getPlaceholder()}
+
+        {children && <div className={s.icons}>{children}</div>}
+        <textarea
+          {...props}
+          value={value}
+          ref={inputRef}
+          onChange={onInputChange}
+          onFocus={nativeFocusEventHandler}
+          onBlur={nativeBlurEventHandler}
+          disabled={disabled}
+          className={classes?.input}
+          style={textareaStyles}
+        />
+      </div>
+      {message && <div className={s.message}>{message}</div>}
+      {error && typeof error === 'string' ? <div className={cn(s.errorMessage, classes?.error)}>{error}</div> : null}
+    </div>
+  );
+}
+
+export const Textarea = forwardRef(TextareaComponent);

41f28e7b6c87a91a3b29fdbbee8919d9f9e5bb59
Alexander CherryTea <Akiyamka@gmail.com>
2023-01-17 12:54:39 +0100

    Reverse headings type names (#56)
    * align headings types names numeration with headings
    
    * align headings types names numeration with headings

--- packages/ui-kit/src/Heading/index.tsx
+++ packages/ui-kit/src/Heading/index.tsx
@@ -4,9 +4,13 @@ type HeadingTypes = 'heading-01' | 'heading-02' | 'heading-03' | 'heading-04' | 
 
 const typeToClass = (type) => `k-font-${type}`;
 
-export function Heading({ children, type }: React.PropsWithChildren<{ type: HeadingTypes }>) {
+export function Heading({
+  children,
+  type,
+  tag,
+}: React.PropsWithChildren<{ type: HeadingTypes; tag?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' }>) {
   if (isValidElement<{ className: string }>(children)) {
     return cloneElement(children, { className: `${typeToClass(type)} ${children.props.className}` });
   }
-  return createElement('h' + type.slice(-1), { className: typeToClass(type) }, children);
+  return createElement(tag ? tag : 'h' + type.slice(-1), { className: typeToClass(type) }, children);
 }

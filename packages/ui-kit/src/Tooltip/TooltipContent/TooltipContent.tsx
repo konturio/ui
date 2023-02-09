461976db5e1367f2c2c829426593cc85cfd0f559
Alexandr Dubinin <akiyamka@gmail.com>
2023-02-09 21:25:55 +0100

    Revert "Revert "feat(tooltip): calculate position with floating-ui (#66)""
    This reverts commit 4a20e7fdaf4d459226094a8e04fb32e399ac7085.

--- packages/ui-kit/src/Tooltip/TooltipContent/TooltipContent.tsx
+++ packages/ui-kit/src/Tooltip/TooltipContent/TooltipContent.tsx
@@ -1,18 +1,20 @@
 import clsx from 'clsx';
 import { Close16 } from '@konturio/default-icons';
+import { forwardRef } from 'react';
 import s from './TooltipContent.module.css';
 import type { PropsWithChildren } from 'react';
 
-export function TooltipContent({
-  children,
-  onClose,
-  className,
-}: PropsWithChildren<{
+type TooltipContentProps = PropsWithChildren<{
   onClose?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
   className?: string;
-}>) {
+}>;
+
+export const TooltipContent = forwardRef<HTMLDivElement, TooltipContentProps>(function TooltipContent(
+  { children, onClose, className }: TooltipContentProps,
+  ref,
+) {
   return (
-    <div className={clsx(s.tooltipContent, className)}>
+    <div ref={ref} className={clsx(s.tooltipContent, className)}>
       {children}
       {onClose && (
         <div className={s.closeIcon} onClick={onClose}>
@@ -21,4 +23,4 @@ export function TooltipContent({
       )}
     </div>
   );
-}
+});

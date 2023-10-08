f07eeccbf8bcd2657a7ea465e461f4693cf4ad7c
dqunbp <dqunbp@gmail.com>
2023-10-08 15:43:04 +0300

    feat: set wrap rules in large button
    wrap text in the large buttons with icon no more than 2 lines or elipsis

--- packages/ui-kit/src/Button/Button.fixture.tsx
+++ packages/ui-kit/src/Button/Button.fixture.tsx
@@ -16,20 +16,20 @@ export default {
     return (
       <div>
         <h3>Primary</h3>
-        <div style={{ margin: '1em', gap: '1em', display: 'flex', flexFlow: 'row nowrap' }}>
+        <div style={{ width: '300px', margin: '1em', gap: '1em', display: 'flex', flexFlow: 'row nowrap' }}>
           <Button onClick={console.log} size={size} disabled={disabled}>
-            Konturio
+            Konturio button
           </Button>
           <Button onClick={console.log} size={size} disabled={disabled} iconAfter={<EyeBallIcon />}>
-            Konturio
+            Calculate MCDA
           </Button>
           <Button onClick={console.log} size={size} disabled={disabled} iconBefore={<EyeBallIcon />}>
-            Konturio
+            Create Layer
           </Button>
           <Button onClick={console.log} size={size} disabled={disabled} iconBefore={<EyeBallIcon />} />
         </div>
         <h3>Invert-Outline</h3>
-        <div style={{ margin: '1em', gap: '1em', display: 'flex', flexFlow: 'row nowrap' }}>
+        <div style={{ width: '250px', margin: '1em', gap: '1em', display: 'flex', flexFlow: 'row nowrap' }}>
           <Button onClick={console.log} size={size} disabled={disabled} variant="invert-outline">
             Konturio
           </Button>
@@ -40,7 +40,7 @@ export default {
             disabled={disabled}
             iconAfter={<EyeBallIcon />}
           >
-            Konturio
+            2 Lines
           </Button>
           <Button
             variant="invert-outline"
@@ -49,7 +49,7 @@ export default {
             disabled={disabled}
             iconBefore={<EyeBallIcon />}
           >
-            Konturio
+            3 lines wrap text here
           </Button>
           <Button
             variant="invert-outline"
@@ -62,12 +62,6 @@ export default {
         <h3>Outline</h3>
         <div style={{ margin: '1em', gap: '1em', display: 'flex', flexFlow: 'row nowrap' }}>
           <Button variant="invert" onClick={console.log} size={size} disabled={disabled}>
-            Konturio
-          </Button>
-          <Button variant="invert" onClick={console.log} size={size} disabled={disabled} iconAfter={<EyeBallIcon />}>
-            Konturio
-          </Button>
-          <Button variant="invert" onClick={console.log} size={size} disabled={disabled} iconBefore={<EyeBallIcon />}>
             Konturio
           </Button>
           <Button variant="invert" onClick={console.log} size={size} disabled={disabled} iconBefore={<EyeBallIcon />} />

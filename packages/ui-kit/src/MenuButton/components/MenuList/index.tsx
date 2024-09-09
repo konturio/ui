2008af1e988ac53ab1081c3ca35e63cf8995de0c
Aliaksandr Baranau <abaranau.dev@gmail.com>
2024-09-09 16:42:05 +0200

    chore: remove redundant prop definition

--- packages/ui-kit/src/MenuButton/components/MenuList/index.tsx
+++ packages/ui-kit/src/MenuButton/components/MenuList/index.tsx
@@ -11,7 +11,6 @@ import type { ForwardRefComponent } from '../../../utils/component-helpers/polym
 export interface MenuListProps {
   portal?: boolean;
   children: React.ReactNode;
-  className?: string;
   classes?: {
     popover?: string;
   };

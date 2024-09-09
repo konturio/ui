7f3153988c21e5b8b3e8ddabbf8ab8a4f52fa8a8
Aliaksandr Baranau <abaranau.dev@gmail.com>
2024-09-09 16:32:56 +0200

    feat(MenuButton): 14063 Allow passing className for MenuPopover

--- packages/ui-kit/src/MenuButton/components/MenuList/index.tsx
+++ packages/ui-kit/src/MenuButton/components/MenuList/index.tsx
@@ -11,9 +11,13 @@ import type { ForwardRefComponent } from '../../../utils/component-helpers/polym
 export interface MenuListProps {
   portal?: boolean;
   children: React.ReactNode;
+  className?: string;
+  classes?: {
+    popover?: string;
+  };
 }
 
-export const MenuList = React.forwardRef(({ portal = true, className, ...props }, forwardedRef) => {
+export const MenuList = React.forwardRef(({ portal = true, className, classes, ...props }, forwardedRef) => {
   const dynamicClasses = cn(
     {
       [style.menuList]: true,
@@ -22,7 +26,7 @@ export const MenuList = React.forwardRef(({ portal = true, className, ...props }
   );
 
   return (
-    <MenuPopover portal={portal}>
+    <MenuPopover className={classes?.popover} portal={portal}>
       <MenuItems {...props} ref={forwardedRef} className={dynamicClasses} />
     </MenuPopover>
   );
